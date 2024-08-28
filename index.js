ca30509904e91bbc969a3933ba0f89f074659151
Cristina Aguilera <developer.c.93.c@gmail.com>
2024-08-28 18:41:02 +0200

    primeros ejercicios

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+let tongueTwister = s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5
 
 // Print out the concatenated string
 
-
+console.log(tongueTwister)
 
 
 /*******************************************
@@ -23,9 +24,12 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+const part1U =part1.slice(0, part1.length-1) + part1[part1.length-1].toUpperCase()
+const part2U = part2.slice(0, part2.length-1) + part2[part2.length-1].toUpperCase()
+const result = part1U + part2U
 
 // Print the cameLtaiL-formatted string
-
+console.log(result)
 
 
 
